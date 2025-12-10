91375fde544f559dde6d83d3bbf91d5d457ee54b
Peter Cornelis <peter.cornelis86@gmail.com>
2025-12-10 22:02:54 +0100

    init lang files with nav translations

---
+++ lang/en/messages.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'nav' => [
+        'about' => 'About me',
+        'skills' => 'Skills',
+        'experience' => 'Experience',
+        'projects' => 'Projects',
+        'contact' => 'Contact',
+        'aria_toggle_nl' => 'Nederlands',
+        'aria_toggle_eng' => 'English',
+        'aria_toggle_dark' => 'Use dark theme',
+        'aria_toggle_light' => 'Use light theme',
+        'aria_close' => 'close menu'
+    ],
+];
---
+++ lang/nl/messages.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'nav' => [
+        'about' => 'Over mij',
+        'skills' => 'Vaardigheden',
+        'experience' => 'Ervaring',
+        'projects' => 'Projecten',
+        'contact' => 'Contact',
+        'aria_toggle_nl' => 'Nederlands',
+        'aria_toggle_eng' => 'English',
+        'aria_toggle_dark' => 'Gebruik donker theme',
+        'aria_toggle_light' => 'Gebruik licht theme',
+        'aria_close' => 'close menu'
+    ],
+];
