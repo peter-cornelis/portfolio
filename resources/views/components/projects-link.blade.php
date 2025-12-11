a74dd2c6e10a7afcbf8e22ca0712faf9a3fbd31c
Peter Cornelis <peter.cornelis86@gmail.com>
2025-12-11 14:34:12 +0100

    improved colors for accessability

--- resources/views/components/projects-link.blade.php
+++ resources/views/components/projects-link.blade.php
@@ -1,7 +1,7 @@
 @props(['href', 'list' => false])
 
-<li class="bg-green hover:bg-green/80 first:rounded-bl-xl only:rounded-b-xl last:rounded-br-xl border-t first:border-r-transparent border border-black/15 border-t-white/20 duration-300">
-    <a href="{{ $href }}" target="_blank" class="block text-center text-mainLight font-semibold {{ !$list ? 'py-3' : '' }}">
+<li class="bg-purple hover:bg-purple/90 dark:bg-green dark:hover:bg-green/90 first:rounded-bl-xl only:rounded-b-xl last:rounded-br-xl border-t first:border-r-transparent border border-black/15 border-t-white/20 duration-300">
+    <a href="{{ $href }}" target="_blank" class="block text-lg text-center text-mainLight font-semibold {{ !$list ? 'py-3' : '' }}">
         {{ $slot }}
     </a>
 </li>
