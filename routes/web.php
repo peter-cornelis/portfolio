b588e77f05732751036b88888cd444261acd4ef9
Peter Cornelis <peter.cornelis86@gmail.com>
2025-12-12 13:32:24 +0100

    cleanup controller

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use App\Http\Controllers\PortfolioController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PortfolioController::class, 'index']);
-Route::post('/ai-chat', [PortfolioController::class, 'aiChat']);
+Route::get('/', function () {
+    return view('home');
+});
