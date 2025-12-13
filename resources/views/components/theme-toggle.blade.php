73cf9737435201c6343fabc2b15a44c9710b7852
Peter Cornelis <peter.cornelis86@gmail.com>
2025-12-13 23:05:05 +0100

    add system theme if no theme is set

--- resources/views/components/theme-toggle.blade.php
+++ resources/views/components/theme-toggle.blade.php
@@ -1,12 +1,27 @@
 <button 
     class="text-mainLight py-2 rounded-full self-center hover:scale-125 transition-all duration-300 cursor-pointer material-symbols-outlined"
-    x-data="{ dark: localStorage.getItem('theme') === 'dark', switching: false }"
-    x-init="document.documentElement.classList.toggle('dark', dark);
-            $watch('dark', value => {
-                localStorage.setItem('theme', value ? 'dark' : 'light');
-                document.documentElement.classList.toggle('dark', value);
-            })"
-    @click="switching = true; setTimeout(() => { dark = !dark; switching = false; }, 200)"
+    x-data="{ 
+        theme: localStorage.getItem('theme'),
+        dark: false, 
+        switching: false 
+    }"
+    x-init="
+        if (theme === null) {
+            systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';         //detect OS level dark mode onload
+            localStorage.setItem('theme', systemTheme);
+        }
+
+        dark = theme === 'dark';
+        document.documentElement.classList.toggle('dark', theme === 'dark');
+
+        $watch('theme', value => {
+            dark = value === 'dark';
+            localStorage.setItem('theme', value);
+            document.documentElement.classList.toggle('dark', value === 'dark');
+        })
+        dark = theme === 'dark';"
+
+    @click="switching = true; setTimeout(() => { theme = theme === 'dark' ? 'light' : 'dark'; switching = false; }, 200)"
     :class="switching ? 'opacity-0 scale-0 rotate-180' : 'opacity-100 scale-100 rotate-0'"
     x-text="dark ? 'dark_mode' : 'brightness_empty'"
 ></button>
