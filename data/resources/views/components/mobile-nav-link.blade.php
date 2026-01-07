1c9bec69e393eff2519db85e29d0348e1d26a34f
Peter Cornelis <peter.cornelis86@gmail.com>
2026-01-07 21:21:39 +0100

    tweak mobile nav

--- data/resources/views/components/mobile-nav-link.blade.php
+++ data/resources/views/components/mobile-nav-link.blade.php
@@ -1,9 +1,9 @@
 @props(['href', 'section' => null])
 
-<li>
+<li class="sm:last:[&>a]:rounded-bl-xl">
     <a 
         href="{{ $href }}" 
-        class="block text-lg font-semibold text-mainLight py-5 px-8 hover:bg-white/20 ease-in-out bg-size-[32px] bg-left bg-no-repeat transition-colors duration-200"
+        class="block text-lg font-semibold text-mainLight py-5 px-8 hover:bg-white/20 bg-size-[4rem_2rem] bg-left bg-no-repeat transition-colors ease-in-out duration-200"
         :class=" {'bg-[linear-gradient(90deg,var(--color-mainLight)_8%,transparent_8%)]' : currentSection === '{{ $section }}' } "
     >
         {{ $slot }}
