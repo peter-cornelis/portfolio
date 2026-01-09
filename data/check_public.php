d3f1058e69d163e769f7a418b9da0d537d65b7da
Peter Cornelis <peter.cornelis86@gmail.com>
2026-01-09 12:05:01 +0100

    merged development branch

--- data/check_public.php
+++
@@ -1,5 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require __DIR__.'/bootstrap/app.php';
-echo $app->make('path.public') . PHP_EOL;
-echo (file_exists($app->make('path.public').'/build/manifest.json') ? 'OK' : 'MISSING') . PHP_EOL;
